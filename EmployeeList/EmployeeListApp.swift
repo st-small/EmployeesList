ee95cde649d8904d73575ea1df66cc31dfbc7303
Stanly Shianovskiy <st-small@rambler.ru>
2023-04-13 17:55:29 +0300

    Initial Commit

---
+++ EmployeeList/EmployeeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeListApp.swift
+//  EmployeeList
+//
+//  Created by Stanly Shiyanovskiy on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
